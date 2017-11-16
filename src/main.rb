b611b3b36ca3236598603b01ad97fbad0fa21a52
0x306e <3so.d.na@gmail.com>
2017-11-16 22:44:46 +0900

    Create to readable format.

--- src/main.rb
+++ src/main.rb
@@ -12,13 +12,36 @@ bot.command :ping do |event|
   event.send('pong')
 end
 
+bot.command :help do |event|
+  event.send(<<"EOS")
+Command usage
+!ping : return 'pong'
+!ctime : Display Cetus time status
+EOS
+end
+
 bot.command :ctime do |event|
+  unixTime = Time.now.to_i
+
+  # get Cetus time
   uri = URI.parse('http://content.warframe.com/dynamic/worldState.php')
   json = Net::HTTP.get(uri)
   raw = JSON.parse(json)
-  Activation = raw['SyndicateMissions']['Activation']['$date']['numberLong']
-  Expiry = raw['SyndicateMissions'][9]['Expiry']['$date']['$numberLong']
-  event.send(Activation, Expiry)
+  Activation = raw['SyndicateMissions'][9]['Activation']['$date']['$numberLong'].to_i
+  Expiry = raw['SyndicateMissions'][9]['Expiry']['$date']['$numberLong'].to_i
+
+  State = 'Night'
+  remain = (Expiry / 1000) - unixTime
+
+  if remain > 3000
+    State = 'Day'
+    remain = remain - 3000
+  end
+  hour = remain / 3600
+  minutes = (remain % 3600) / 60
+  seconds = remain % 60
+
+  event.send("Now in #{State}, left to #{hour}h #{minutes}m #{seconds}s.")
 end
 
 bot.run
