77de950edc0cd93d4b01e2769c4d00c30bc6f7fe
leno3s <3so.d.na@gmail.com>
2018-11-29 11:34:26 +0900

    ping, ctime, gachaの実装

--- src/bot.rb
+++ src/bot.rb
@@ -1,9 +1,11 @@
 require 'discordrb'
 require 'dotenv'
+require_relative './world_state'
 
 module CetusBot
     class Bot
         @bot
+        @state
 
         def initialize()
             Dotenv.load('.env')
@@ -12,12 +14,56 @@ module CetusBot
                 client_id: ENV['DISCORD_CLIENT_ID'],
                 prefix: '!',
             )
-            self.setting
+            @state = CetusBot::WorldState.new
+            self.setting()
         end
 
-        def setting()
+        def run()
+            @bot.run()
+        end
+
+        def setting() # command setups
             @bot.command :ping do |event|
-                event.respond("pong (#{Time.now - event.timeStamp}sec. taken)")
+                event.respond("pong (#{Time.now - event.timeStamp}sec)")
+            end
+
+            @bot.command :ctime do |event|
+                remain = @state.eidlon_time() / 1000 - Time.now.to_i
+                state = case remain
+                        when 0..(50*60)
+                            'Night'
+                        else
+                            'Day'
+                        end
+
+                if remain < 0
+                    remain += 150 * 60
+                end
+                if remain > 3000
+                    state = 'Day'
+                    remain = remain - 3000
+                end
+                h = remain / 3600
+                m = (remain % 3600) / 60
+                s = remain % 60
+
+                event.respond("Now in #{state}, left to #{h}h #{m}m #{s}s.")
+            end
+
+            @bot.command :gacha do |event|
+                j = nil
+                File.open('./FrameList.json', 'r') do |f|
+                    j = JSON.load(f)
+                end
+                size = j['frames'].length()
+                index = Random.new.rand(size)
+                event.send(j['frames'][index])
+            end
+
+            @bot.command :dice do |event, max|
+                max ||= 6
+                val = max.to_i
+                event.send(rand(1..val))
             end
         end
     end
