67eec7dd15da8579567e85d17a80b4005d8d98f9
0x306e <3so.d.na@gmail.com>
2018-12-11 02:23:56 +0900

    api/alerts 雑実装

--- src/api/alerts.rb
+++ src/api/alerts.rb
@@ -1,5 +1,38 @@
+require '../world_state'
+
 module CetusBot::API
   class Alerts
+    class Alert
+      def initialize(json)
+        @solNodes = File.open(File.expand_path('solNodes.json', CetusBot::WorldState::DATA_DIR)) do |f|
+          JSON.load(f)
+        end
+        @missionTypes = File.open(File.expand_path('missionTypes.json', CetusBot::WorldState::DATA_DIR)) do |f|
+          JSON.load(f)
+        end
+        @factionData = File.open(File.expand_path('factionData.json', CetusBot::WorldState::DATA_DIR)) do |f|
+          JSON.load(f)
+        end
+        @activation = json['Activation']['$date']['$numberLong']
+        @expiry = json['Expiry']['$date']['$numberLong']
+        @missionType = @missionTypes[json['MissionInfo']['missionType']]
+        @faction = @factionData[json['MissionInfo']['faction']]
+        @node = @solNodes[json['MissionInfo']['location']]
+        @reward = json['MissionInfo']['MissionReward']
+      end
+      attr_reader :activation, :expiry, :missionType, :faction, :node, :reward
+    end
 
+    def initialize
+      @state = CetusBot::WorldState.instance
+    end
+
+    def now_list
+      list = []
+      @state.json['Alerts'].each {|j|
+        list.append(Alert.new(j))
+      }
+      return list
+    end
   end
 end
